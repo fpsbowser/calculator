1f36f1eff7844465301ea197cfdc237b2b96f5d6
Dakota <prattdakota12@gmail.com>
2021-12-23 14:14:11 -0500

    Version 1. Working operations but still needs some work.

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
+// Display Value
+let displayValue = document.querySelector('.display');
+
+let firstValue = 0;
+let operatorValue = '';
+let solution = '';
+
+// Event Listener for all calculator buttons
+let button = document.querySelector('.button-container');
+button.addEventListener('click', (e) => {
+
+    const isButton = e.target.nodeName === 'button';
+    if (!isButton && isNaN(e.target.textContent) == false) {
+        console.log(e.target.id);
+        displayValue.textContent += e.target.textContent;
+    } else if (!isButton && e.target.textContent == '÷') {
+        firstValue = displayValue.textContent;
+        operatorValue = '/';
+        displayValue.textContent = '';
+        console.log(firstValue);
+    } else if (!isButton && e.target.textContent == 'x') {
+        firstValue = displayValue.textContent;
+        operatorValue = 'x'
+        displayValue.textContent = '';
+    } else if (!isButton && e.target.textContent == '-') {
+        firstValue = displayValue.textContent;
+        operatorValue = '-';
+        displayValue.textContent = '';
+    } else if (!isButton && e.target.textContent == '+') {
+        firstValue = displayValue.textContent;
+        operatorValue = '+'
+        displayValue.textContent = '';
+    } else if (!isButton && e.target.textContent == '=') {
+        secondValue = displayValue.textContent;
+        operate(firstValue, operatorValue, displayValue.textContent); 
+    } else if (!isButton && e.target.textContent == 'CLR') {
+        displayValue.textContent = '';
+    }
+})
+
+    /* FUNCTIONS */
+
 // Add function
 function addition(num1, num2) {
-    let sum = num1 + num2;
+    let sum = Number(num1) + Number(num2);
     console.log(sum);
+    displayValue.textContent = `${sum}`;
 }
 
 // Subtract function
 function subtract(num1, num2) {
     let subtractValue = num1 - num2;
     console.log(subtractValue);
+    displayValue.textContent = `${subtractValue}`;
 }
 
 // Multiply function
 function multiply(num1, num2) {
     let multiplyValue = num1 * num2;
     console.log(multiplyValue);
+    displayValue.textContent = `${multiplyValue}`;
 }
 
 // Divide function
 function divide(num1, num2) {
     let divideValue = num1 / num2;
     console.log(divideValue);
+    displayValue.textContent = `${divideValue}`;
 }
 
 // Operate function
@@ -28,7 +74,7 @@ function operate(number1, operator, number2) {
         addition(number1, number2);
     } else if (operator == '-') {
         subtract(number1, number2);
-    } else if (operator == '*') {
+    } else if (operator == 'x') {
         multiply(number1, number2);
     } else if (operator == '/') {
         divide(number1, number2);
@@ -36,8 +82,3 @@ function operate(number1, operator, number2) {
         console.log('Invalid operator!');
     }
 }
-
-operate(5,'+',4);
-operate(5,'-',4);
-operate(5,'*',4);
-operate(6,'/',3);

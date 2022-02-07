0ebd12bcb11307dd88d5178819ccf0ce2e1dd58c
Dakota <prattdakota12@gmail.com>
2022-02-07 15:07:19 -0500

    Bug fixes and added functionality for chaining different operators

--- script.js
+++ script.js
@@ -1,27 +1,126 @@
+// Display Value
+let displayValue = document.querySelector('.display');
+
+let value = 0;
+let firstValue = 0;
+let secondValue = 0;
+let operatorValue = '';
+let solution = '';
+let shouldReset = false;
+let equalsPressed = false;
+
+// Event Listener for all calculator buttons
+let button = document.querySelector('.button-container');
+button.addEventListener('click', (e) => {
+
+    const isButton = e.target.nodeName === 'button';
+
+    switch (!isButton && e.target.textContent) {
+        case '÷':
+            if (operatorValue != '') {
+                operate(firstValue, operatorValue, displayValue.textContent);
+                operatorValue = '/'
+                firstValue = displayValue.textContent;
+                shouldReset = true;
+                break;
+            }
+            firstValue = displayValue.textContent;
+            operatorValue = '/';
+            displayValue.textContent = '';
+            break;
+        case 'x':
+            if (operatorValue != '') {
+                operate(firstValue, operatorValue, displayValue.textContent);
+                operatorValue = 'x';
+                firstValue = displayValue.textContent;
+                shouldReset = true;
+                break;
+            }
+            firstValue = displayValue.textContent;
+            operatorValue = 'x';
+            displayValue.textContent = '';
+            break;
+        case '-':
+            if (operatorValue != '') {
+                operate(firstValue, operatorValue, displayValue.textContent);
+                operatorValue = '-'
+                firstValue = displayValue.textContent;
+                shouldReset = true;
+                break;
+            }
+            firstValue = displayValue.textContent;
+            operatorValue = '-';
+            displayValue.textContent = '';
+            break;
+        case '+':
+            if (operatorValue != '') {
+                operate(firstValue, operatorValue, displayValue.textContent);
+                operatorValue = '+'
+                firstValue = displayValue.textContent;
+                shouldReset = true;
+                break;
+            }
+            firstValue = displayValue.textContent;
+            operatorValue = '+';
+            displayValue.textContent = '';
+            break;
+        case '=':
+            operate(firstValue, operatorValue, displayValue.textContent);
+            operatorValue = '';
+            break;
+        case 'CLR':
+            firstValue = 0;
+            shouldReset = false;
+            secondValue = 0;
+            operatorValue = '';
+            displayValue.textContent = '';
+            break;
+        default:
+            if (shouldReset == true) {
+                displayValue.textContent = '';
+            }
+            shouldReset = false;
+            displayValue.textContent += e.target.textContent;
+    }
+})
+
+    /* FUNCTIONS */
+
 // Add function
 function addition(num1, num2) {
-    let sum = num1 + num2;
-    console.log(sum);
-    return sum;
+    let sum = Number(num1) + Number(num2);
+    displayValue.textContent = `${sum}`;
+    value = sum;
 }
 
 // Subtract function
 function subtract(num1, num2) {
-    console.log(num1, num2);
     let subtractValue = num1 - num2;
-    return subtractValue;
+    if (subtractValue % 1 != 0) {
+        subtractValue = (num1 - num2).toFixed(2);
+    }
+    displayValue.textContent = `${subtractValue}`;
+    value = subtractValue;
 }
 
 // Multiply function
 function multiply(num1, num2) {
     let multiplyValue = num1 * num2;
-    return multiplyValue;
+    if (multiplyValue % 1 != 0) {
+        multiplyValue = (num1 / num2).toFixed(2);
+    }
+    displayValue.textContent = `${multiplyValue}`;
+    value = multiplyValue;
 }
 
 // Divide function
 function divide(num1, num2) {
     let divideValue = num1 / num2;
-    return divideValue;
+    if (divideValue % 1 != 0) {
+        divideValue = (num1 / num2).toFixed(2);
+    }
+    displayValue.textContent = `${divideValue}`;
+    value = divideValue;
 }
 
 // Operate function
@@ -30,11 +129,11 @@ function operate(number1, operator, number2) {
         addition(number1, number2);
     } else if (operator == '-') {
         subtract(number1, number2);
-    } else if (operator == '*') {
+    } else if (operator == 'x') {
         multiply(number1, number2);
     } else if (operator == '/') {
         divide(number1, number2);
     } else {
-        'Invalid operator!'
+        console.log('Invalid operator!');
     }
 }
